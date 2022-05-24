7a8896e4aaf6bd5914a2d4c493689000421f425a
sanjay <89021590+sanjayku0@users.noreply.github.com>
2022-05-24 17:15:25 +0530

    new api

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000 
+io.on("connection", socket => {
+    console.log("a user connected :D");
+    socket.on("chat message", msg => {
+      console.log(msg);
+      io.emit("chat message", msg);
+    });
+  });   
+
+  server.listen(port, () => console.log("server running on port:" + port));
